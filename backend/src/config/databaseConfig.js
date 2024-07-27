fc972833c862729399b88a5271bd5ecd4402f9aa
prettygenius <devprettygenius@gmail.com>
2024-07-27 14:25:28 -0500

    updated server and config file for mongo connection

--- backend/src/config/databaseConfig.js
+++ backend/src/config/databaseConfig.js
@@ -15,9 +15,8 @@ mongoose.set('strictQuery', true);
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            //useNewUrlParser: true,
-           // useUnifiedTopology: true,
+        await mongoose.connect(process.env.MONGODB_URI, {
+        
         });
         await seedUsers();
         await seedMenu();
